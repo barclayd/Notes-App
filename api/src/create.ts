93cf14b7b2e730a570dba9f6b8c2329318cb3125
Daniel Barclay <barclaysd@me.com>
2020-12-16 12:38:52 +0000

    LambdaService.ts and refactor create lambda to use LambdaService and dynamoDBService

--- api/src/create.ts
+++ api/src/create.ts
@@ -1,30 +1,26 @@
 import { v4 as uuid } from 'uuid';
-import AWS from 'aws-sdk';
 import { APIGatewayEvent, Context } from 'aws-lambda';
 import { DocumentClient } from 'aws-sdk/clients/dynamodb';
-
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
+import { dynamoDBService } from './services/DynamoDBService';
+import { LambdaService } from './services/LambdaService';
+import { Lambda } from './types/lambda';
 
 export async function main(event: APIGatewayEvent, context: Context) {
-  const data = JSON.parse(event.body);
+  const lambda: Lambda = async ({ event, context }) => {
+    const data = JSON.parse(event.body);
 
-  const params: DocumentClient.PutItemInput = {
-    TableName: process.env.tableName,
-    Item: {
-      userId: '123',
-      noteId: uuid(),
-      content: data.content,
-      attachment: data.attachment,
-      createdAt: Date.now(),
-    },
-  };
-
-  try {
-    await dynamoDB.put(params).promise();
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: error.message }),
+    const params: DocumentClient.PutItemInput = {
+      TableName: process.env.tableName,
+      Item: {
+        userId: '123',
+        noteId: uuid(),
+        content: data.content,
+        attachment: data.attachment,
+        createdAt: Date.now(),
+      },
     };
-  }
+    await dynamoDBService.put(params);
+    return params.Item;
+  };
+  return await new LambdaService(lambda, event, context).execute();
 }
