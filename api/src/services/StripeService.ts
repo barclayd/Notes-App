a569f20a181848f630788e40d3c8f1be67e7f4d6
Daniel Barclay <barclaysd@me.com>
2020-12-17 12:30:44 +0000

    StripeService

---
+++ api/src/services/StripeService.ts
@@ -0,0 +1,22 @@
+import Stripe from 'stripe';
+
+export class StripeService {
+  private stripe: Stripe;
+
+  constructor(secretKey = process.env.stripeSecretKey) {
+    this.stripe = new Stripe(secretKey, {
+      apiVersion: '2020-08-27',
+    });
+  }
+
+  public async create(amount?: number, description?: string, source?: string) {
+    await this.stripe.charges.create({
+      amount,
+      description,
+      source,
+      currency: 'gbp',
+    });
+  }
+}
+
+export const stripeService = new StripeService();
