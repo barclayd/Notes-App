dc449ccc2a3ca69d640928ef120a67637629379b
Daniel Barclay <barclaysd@me.com>
2020-12-22 12:44:16 +0000

    Add very basic routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { NavBar } from './NavBar';
+import Routes from './Routes';
 
 function App() {
   return (
@@ -19,7 +20,7 @@ function App() {
       <main>
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           <div className="px-4 py-6 sm:px-0">
-            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+            <Routes />
           </div>
         </div>
       </main>
--- frontend/src/NavBar.tsx
+++ frontend/src/NavBar.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export const NavBar = () => {
   const [showProfileDropdown, setProfileDropdown] = useState(false);
@@ -12,38 +13,40 @@ export const NavBar = () => {
               className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               aria-expanded="false"
             >
-              <span className="sr-only">Open main menu</span>
+              <Link to="/">
+                <span className="sr-only">Open main menu</span>
 
-              <svg
-                className="block h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-              <svg
-                className="hidden h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
+                <svg
+                  className="block h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+                <svg
+                  className="hidden h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </Link>
             </button>
           </div>
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
@@ -62,21 +65,27 @@ export const NavBar = () => {
             <div className="hidden sm:block sm:ml-6">
               <div className="flex space-x-4">
                 <button className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium focus:outline-none">
-                  Dashboard
+                  <Link to='/'>Dashboard</Link>
                 </button>
                 <button className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium focus:outline-none">
-                  Notes
+                  <Link to='/notes'>Notes</Link>
                 </button>
                 <button className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium focus:outline-none">
-                  Shared
+                  <Link to='/shared'>Shared</Link>
                 </button>
                 <button className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium focus:outline-none">
-                  Settings
+                  <Link to='/settings'>Settings</Link>
                 </button>
               </div>
             </div>
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <button className="bg-blue-300 text-white px-3 mx-1 py-2 rounded-md text-sm font-medium focus:outline-none">
+              <Link to="/sign-in">Sign in</Link>
+            </button>
+            <button className="border-blue-300 border-solid text-white border-2 mx-3 px-3 py-2 rounded-md text-sm font-medium focus:outline-none">
+              <Link to="/sign-up">Sign up</Link>
+            </button>
             <button className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
               <span className="sr-only">View notifications</span>
               <svg
@@ -154,9 +163,7 @@ export const NavBar = () => {
           <button className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium focus:outline-none">
             Projects
           </button>
-          <button
-            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium focus:outline-none"
-          >
+          <button className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium focus:outline-none">
             Calendar
           </button>
         </div>
