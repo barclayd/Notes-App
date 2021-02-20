1f45d3bb2f5b3fbdc8c0ed604671e53d89364364
Daniel Barclay <barclaysd@me.com>
2021-02-20 19:09:06 +0000

    DynamoDBStack

--- api/infrastructure/lib/index.ts
+++ api/infrastructure/lib/index.ts
@@ -1,8 +1,6 @@
-import MyStack from "./MyStack";
-import * as sst from "@serverless-stack/resources";
+import DynamoDBStack from './DynamoDBStack';
+import * as sst from '@serverless-stack/resources';
 
 export default function main(app: sst.App): void {
-  new MyStack(app, "my-stack");
-
-  // Add more stacks
+  new DynamoDBStack(app, 'dynamodb');
 }
