40735442ba4a091b9009c023ad3189129bdc5703
Daniel Barclay <barclaysd@me.com>
2021-02-21 12:49:57 +0000

    Add reorganised files

---
+++ infrastructure/lib/DynamoDBStack.ts
@@ -0,0 +1,33 @@
+import { CfnOutput } from '@aws-cdk/core';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import * as sst from '@serverless-stack/resources';
+
+export default class DynamoDBStack extends sst.Stack {
+  constructor(scope: sst.App, id: string, props?: sst.StackProps) {
+    super(scope, id, props);
+
+    const app = scope.node.root as sst.App;
+
+    const table = new dynamodb.Table(this, 'Table', {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      sortKey: {
+        name: 'noteId',
+        type: dynamodb.AttributeType.STRING,
+      },
+      partitionKey: {
+        name: 'userId',
+        type: dynamodb.AttributeType.STRING,
+      },
+    });
+
+    new CfnOutput(this, 'TableName', {
+      value: table.tableName,
+      exportName: app.logicalPrefixedName('TableName'),
+    });
+
+    new CfnOutput(this, 'TableArn', {
+      value: table.tableArn,
+      exportName: app.logicalPrefixedName('TableArn'),
+    });
+  }
+}
